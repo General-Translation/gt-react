52e3386fd7618780c23d009b6603483a329805db
archie-mckenzie <archie@generaltranslation.com>
2024-08-24 19:16:23 -0700

    clone metadata

--- dist/cjs/dictionary/cloneDictionary.js
+++ dist/cjs/dictionary/cloneDictionary.js
@@ -6,13 +6,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = cloneDictionary;
 const react_1 = __importDefault(require("react"));
 const getEntryMetadata_1 = __importDefault(require("../primitives/rendering/getEntryMetadata"));
+function cloneMetadata(metadata) {
+    if (react_1.default.isValidElement(metadata)) {
+        return react_1.default.cloneElement(metadata);
+    }
+    if (typeof metadata !== 'object' || metadata === null) {
+        return metadata;
+    }
+    const clonedObj = {};
+    for (const key in metadata) {
+        if (metadata.hasOwnProperty(key)) {
+            clonedObj[key] = cloneMetadata(metadata[key]);
+        }
+    }
+    return clonedObj;
+}
 function cloneDictionary(dictionary) {
     const clonedDictionary = {};
     for (const id of Object.keys(dictionary)) {
         const { entry, metadata } = (0, getEntryMetadata_1.default)(dictionary[id]);
         let clonedEntry = react_1.default.isValidElement(entry) ? react_1.default.cloneElement(entry) : structuredClone(entry);
         if (metadata) {
-            let clonedMetadata = structuredClone(metadata);
+            let clonedMetadata = cloneMetadata(metadata);
             clonedDictionary[id] = [clonedEntry, clonedMetadata];
             continue;
         }
--- src/dictionary/cloneDictionary.ts
+++ src/dictionary/cloneDictionary.ts
@@ -1,13 +1,30 @@
 import React from "react";
 import getEntryMetadata from "../primitives/rendering/getEntryMetadata";
 
+function cloneMetadata(metadata: Record<string, any>): Record<string, any> {
+    if (React.isValidElement(metadata)) {
+        return React.cloneElement(metadata);
+    }
+    if (typeof metadata !== 'object' || metadata === null) {
+        return metadata;
+    }
+    const clonedObj: Record<string, any> = {};
+    for (const key in metadata) {
+        if (metadata.hasOwnProperty(key)) {
+            clonedObj[key] = cloneMetadata(metadata[key]);
+        }
+    }
+    return clonedObj;
+}
+
+
 export default function cloneDictionary(dictionary: Record<string, any>) {
     const clonedDictionary: Record<string, any> = {};
     for (const id of Object.keys(dictionary)) {
         const { entry, metadata } = getEntryMetadata(dictionary[id]);
         let clonedEntry = React.isValidElement(entry) ? React.cloneElement(entry) : structuredClone(entry);
         if (metadata) {
-            let clonedMetadata = structuredClone(metadata);
+            let clonedMetadata = cloneMetadata(metadata);
             clonedDictionary[id] = [clonedEntry, clonedMetadata];
             continue;
         }
