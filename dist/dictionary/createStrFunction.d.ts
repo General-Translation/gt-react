3ff442abf601e36b94c44d19c4fedd4cdab1d0e6
archie-mckenzie <archie@generaltranslation.com>
2024-09-03 18:27:51 -0700

    error handling

--- dist/dictionary/createStrFunction.d.ts
+++
@@ -1,5 +0,0 @@
-import { tOptions } from "./createTFunction";
-export default function createStrFunction({ t }: {
-    t: any;
-}): (id: string, options?: tOptions) => any;
-//# sourceMappingURL=createStrFunction.d.ts.map
