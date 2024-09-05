d9262a84561db338f3a6f43ac239acc9c8b9ee97
archie-mckenzie <archie@generaltranslation.com>
2024-09-04 21:17:14 -0700

    isVariableObject

--- src/primitives/rendering/getEntryMetadata.ts
+++ src/primitives/rendering/getEntryMetadata.ts
@@ -1,5 +1,23 @@
 import { isValidElement } from "react";
 
+// check to protect developer from consequences of their actions
+// the only way this would be relevant is either:
+// 1. you are rendering the dictionary on the server before passing it to GTClientProvider (why??? pass it to GTProvider unless you are using an entirely client-side React app)
+// or 2. you are trying to use preprocessed string variables in the dictionary (sneaky!!!)
+const isVariableObject = (obj: any) => {
+    if (typeof obj === 'object') {
+        if (typeof obj.key === 'string') {
+            const keys = Object.keys(obj);
+            if (keys.length === 1) {
+                return true;
+            } else if (keys.length === 2 && typeof obj.variable === 'string') {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 // Extracts metadata if it has been included in the dictionary
 export default function getEntryMetadata(entry: any): {
     entry: any,
@@ -8,7 +26,7 @@ export default function getEntryMetadata(entry: any): {
     let content;
     let metadata;
     if (entry) {
-        if (Array.isArray(entry) && entry.length === 2 && !isValidElement(entry[1]) && typeof entry[1] === 'object') {
+        if (Array.isArray(entry) && entry.length === 2 && !isValidElement(entry[1]) && typeof entry[1] === 'object' && !isVariableObject(entry[1])) {
             content = entry[0];
             metadata = entry[1];
         }
