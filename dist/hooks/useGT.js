19412d7cd3423b66b41f677cbc34034c0f195cdd
archie-mckenzie <archie@generaltranslation.com>
2024-09-14 16:47:50 -0700

    f()

--- dist/hooks/useGT.js
+++ dist/hooks/useGT.js
@@ -29,8 +29,16 @@ function useGT(id) {
     };
     // Get the translation context
     var translate = (0, GTContext_1.default)("useGT('".concat(id, "'): No context provided. You're trying to get the t() function on the client, which can only be done inside a <GTProvider>.")).translate;
-    // Return a translation function if available, otherwise return a no-op function
-    return function (id, options, f) {
+    /**
+    * Translates a dictionary item based on its `id` and options.
+    *
+    * @param {string} [id=''] - The ID of the item in the dictionary to translate.
+    * @param {Record<string, any>} [options={}] - Variables or parameters (e.g., `n`) passed into the translation for dynamic content.
+    * @param {Function} [f] - `f` is executed with `options` as parameters, and its result is translated based on the dictionary value of `id`. You likely don't need this parameter unless you are using `gt-next`.
+    *
+    * @returns {string | JSX.Element}
+    */
+    function t(id, options, f) {
         if (id === void 0) { id = ''; }
         if (options === void 0) { options = {}; }
         var prefixedID = getID(id);
@@ -38,6 +46,8 @@ function useGT(id) {
         if (!translation)
             console.warn("t('".concat(id, "') finding no translation for dictionary item ").concat(prefixedID, " !"));
         return translation;
-    };
+    }
+    ;
+    return t;
 }
 //# sourceMappingURL=useGT.js.map
--- src/hooks/useGT.ts
+++ src/hooks/useGT.ts
@@ -34,14 +34,26 @@ export default function useGT(id: string = ''):
         `useGT('${id}'): No context provided. You're trying to get the t() function on the client, which can only be done inside a <GTProvider>.`
     );
 
-    // Return a translation function if available, otherwise return a no-op function
-    return (id: string = '', options: {
-        n?: number,
-        [key: string]: any
-    } = {}, f?: Function) => {
+   
+    /**
+    * Translates a dictionary item based on its `id` and options.
+    * 
+    * @param {string} [id=''] - The ID of the item in the dictionary to translate.
+    * @param {Record<string, any>} [options={}] - Variables or parameters (e.g., `n`) passed into the translation for dynamic content.
+    * @param {Function} [f] - `f` is executed with `options` as parameters, and its result is translated based on the dictionary value of `id`. You likely don't need this parameter unless you are using `gt-next`.
+    * 
+    * @returns {string | JSX.Element}
+    */
+    function t(
+        id: string = '', 
+        options: Record<string, any> = {}, 
+        f?: Function
+    ): string | JSX.Element {
         const prefixedID = getID(id);
         const translation = translate(prefixedID, options, f);
         if (!translation) console.warn(`t('${id}') finding no translation for dictionary item ${prefixedID} !`)
         return translation;
     };
+
+    return t;
 }
