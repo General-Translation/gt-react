b0922a892e1b63bbf72ffbf66b793b3aa8bc749c
archie-mckenzie <archie@generaltranslation.com>
2024-09-17 08:07:25 -0700

    gt-next

--- dist/utils/getI18NConfig.js
+++ dist/utils/getI18NConfig.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = getI18NConfig;
 function getI18NConfig() {
-    console.log('globalThis', globalThis.__GENERALTRANSLATION__ ? true : false);
+    if (!globalThis.__GENERALTRANSLATION__) {
+        throw new Error('Unable to access gt-next configuration.');
+    }
     return globalThis.__GENERALTRANSLATION__;
 }
 //# sourceMappingURL=getI18NConfig.js.map
--- src/utils/getI18NConfig.ts
+++ src/utils/getI18NConfig.ts
@@ -4,7 +4,7 @@ export default function getI18NConfig(): I18NConfiguration {
     if (!(
         globalThis as any
     ).__GENERALTRANSLATION__) {
-        throw new Error(`Unable to access gt-next configuration.`)
+        throw new Error('Unable to access gt-next configuration.')
     }
     return (
         globalThis as any
