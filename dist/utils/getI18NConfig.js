616369a6dd97a5cbf5caf36cdb05663b4ca88de4
archie-mckenzie <archie@generaltranslation.com>
2024-09-17 08:17:34 -0700

    globalThis

--- dist/utils/getI18NConfig.js
+++ dist/utils/getI18NConfig.js
@@ -2,12 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = getI18NConfig;
 function getI18NConfig() {
-    if (!globalThis) {
-        throw new Error('Unable to access globalThis.');
+    if (!globalThis && !global) {
+        throw new Error('Unable to access globalThis for gt-next.');
     }
-    if (!globalThis.__GENERALTRANSLATION__) {
+    var I18NConfig = (globalThis === null || globalThis === void 0 ? void 0 : globalThis.__GENERALTRANSLATION__) || (global === null || global === void 0 ? void 0 : global.__GENERALTRANSLATION__);
+    if (!I18NConfig) {
         throw new Error('Unable to access gt-next configuration.');
     }
-    return (globalThis === null || globalThis === void 0 ? void 0 : globalThis.__GENERALTRANSLATION__) || (global === null || global === void 0 ? void 0 : global.__GENERALTRANSLATION__);
+    return I18NConfig;
 }
 //# sourceMappingURL=getI18NConfig.js.map
--- src/utils/getI18NConfig.ts
+++ src/utils/getI18NConfig.ts
@@ -1,17 +1,16 @@
 import I18NConfiguration from "../config/I18NConfiguration";
 
 export default function getI18NConfig(): I18NConfiguration {
-    if (!globalThis) {
-        throw new Error('Unable to access globalThis.')
+    if (!globalThis && !global) {
+        throw new Error('Unable to access globalThis for gt-next.')
     }
-    if (!(
-        globalThis as any
-    ).__GENERALTRANSLATION__) {
-        throw new Error('Unable to access gt-next configuration.')
-    }
-    return (
+    const I18NConfig = (
         globalThis as any
     )?.__GENERALTRANSLATION__ || (
         global as any
     )?.__GENERALTRANSLATION__;
+    if (!I18NConfig) {
+        throw new Error('Unable to access gt-next configuration.')
+    }
+    return I18NConfig;
 }
