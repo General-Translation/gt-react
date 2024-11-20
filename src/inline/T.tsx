0def57d9250eceec172babe5444cdcefa06de444
Brian Lou <brianlou2002@gmail.com>
2024-11-20 15:20:07 -0800

    Make prettier

--- src/inline/T.tsx
+++ src/inline/T.tsx
@@ -1,25 +1,29 @@
-import React, { Suspense } from "react";
-import { isSameLanguage } from "generaltranslation";
-import useDefaultLocale from "../hooks/useDefaultLocale";
-import useLocale from "../hooks/useLocale";
-import renderDefaultChildren from "../provider/rendering/renderDefaultChildren";
-import { addGTIdentifier, hashReactChildrenObjects, writeChildrenAsObjects } from "../internal";
-import useGTContext from "../provider/GTContext";
-import renderTranslatedChildren from "../provider/rendering/renderTranslatedChildren";
-import { useMemo } from "react";
+import React, { Suspense } from 'react';
+import { isSameLanguage } from 'generaltranslation';
+import useDefaultLocale from '../hooks/useDefaultLocale';
+import useLocale from '../hooks/useLocale';
+import renderDefaultChildren from '../provider/rendering/renderDefaultChildren';
+import {
+  addGTIdentifier,
+  hashReactChildrenObjects,
+  writeChildrenAsObjects,
+} from '../internal';
+import useGTContext from '../provider/GTContext';
+import renderTranslatedChildren from '../provider/rendering/renderTranslatedChildren';
+import { useMemo } from 'react';
 
 /**
  * Translation component that handles rendering translated content, including plural forms.
  * Used with the required `id` parameter instead of `const t = useGT()`.
- * 
+ *
  * @param {string} [id] - Required identifier for the translation string.
  * @param {React.ReactNode} children - The content to be translated or displayed.
  * @param {any} [context] - Additional context used for translation.
  * @param {Object} [props] - Additional props for the component.
  * @returns {JSX.Element} The rendered translation or fallback content based on the provided configuration.
- * 
+ *
  * @throws {Error} If a plural translation is requested but the `n` option is not provided.
- * 
+ *
  * @example
  * ```jsx
  * // Basic usage:
@@ -27,7 +31,7 @@ import { useMemo } from "react";
  *  Hello, <Var name="name"/>!
  * </T>
  * ```
- * 
+ *
  * @example
  * ```jsx
  * // Using plural translations:
@@ -35,86 +39,100 @@ import { useMemo } from "react";
  *  You have <Num/> items
  * </T>
  * ```
- * 
+ *
  */
 export default function T({
-    children, id, ...props
+  children,
+  id,
+  ...props
 }: {
-    children?: any,
-    id: string
-    context?: string,
-    [key: string]: any
+  children?: any;
+  id: string;
+  context?: string;
+  [key: string]: any;
 }): JSX.Element | undefined {
+  if (!children) return undefined;
 
-    if (!children) return undefined;
-    
-    if (!id) {
-        throw new Error(`Client-side <T> with no provided 'id' prop. Children: ${children}`)
-    }
-
-    const { variables, variablesOptions } = props;
-
-    const { translations } = useGTContext(
-        `<T id="${id}"> used on the client-side outside of <GTProvider>`
+  if (!id) {
+    throw new Error(
+      `Client-side <T> with no provided 'id' prop. Children: ${children}`
     );
+  }
 
-    const locale = useLocale();
-    const defaultLocale = useDefaultLocale();
+  const { variables, variablesOptions } = props;
 
-    const taggedChildren = useMemo(() => addGTIdentifier(children), [children])
+  const { translations } = useGTContext(
+    `<T id="${id}"> used on the client-side outside of <GTProvider>`
+  );
 
-    const translationRequired: boolean = (() => {
-        if (!locale) return false;
-        if (isSameLanguage(locale, defaultLocale)) return false;
-        return true;
-    })();
+  const locale = useLocale();
+  const defaultLocale = useDefaultLocale();
 
-    if (!translationRequired) {
-        return renderDefaultChildren({
-            children: taggedChildren,
-            variables, variablesOptions, defaultLocale
-        }) as JSX.Element;
-    }
+  const taggedChildren = useMemo(() => addGTIdentifier(children), [children]);
 
-    // Do translation
-    const context = props.context;
-    const [childrenAsObjects, key] = useMemo(() => {
-        const childrenAsObjects = writeChildrenAsObjects(taggedChildren);
-        const key: string = hashReactChildrenObjects(context ? [childrenAsObjects, context] : childrenAsObjects);
-        return [childrenAsObjects, key];
-    }, [context, taggedChildren]);
+  const translationRequired: boolean = (() => {
+    if (!locale) return false;
+    if (isSameLanguage(locale, defaultLocale)) return false;
+    return true;
+  })();
 
-    const translation = translations[id];
-    
-    if (translation?.promise) {
-        throw new Error(`<T id="${id}">, "${id}" is also used as a key in the dictionary. Don't give <T> components the same ID as dictionary entries.`)
-    }
-    if (!translation || !translation.t || translation.k !== key) {
-        
-        if (process?.env.NODE_ENV === 'development' || process?.env.NODE_ENV === 'test') {
-            throw new Error(
-                `<T id="${id}"> is used in a client component without a valid corresponding translation. This can cause Next.js hydration errors.`
-                + `\n\nYour current environment: "${process?.env.NODE_ENV}". In production, this error will display as a warning only, and content will be rendered in your default language.`
-                + `\n\nTo fix this error, consider using a getGT() dictionary pattern or pushing translations from the command line in advance.`
-            );
-        }
-
-        console.warn(`<T id="${id}"> is used in a client component without a valid corresponding translation.`);
-        const defaultChildren = renderDefaultChildren({
-            children: taggedChildren,
-            variables, variablesOptions, defaultLocale
-        }) as JSX.Element;
-
-        // The suspense exists here for hydration reasons
-        return (
-            <Suspense fallback={defaultChildren}>
-                {defaultChildren}
-            </Suspense>
-        );
-    }
-   
-    return renderTranslatedChildren({
-        source: taggedChildren, target: translation.t,
-        variables, variablesOptions, locales: [locale, defaultLocale]
+  if (!translationRequired) {
+    return renderDefaultChildren({
+      children: taggedChildren,
+      variables,
+      variablesOptions,
+      defaultLocale,
     }) as JSX.Element;
+  }
+
+  // Do translation
+  const context = props.context;
+  const [childrenAsObjects, key] = useMemo(() => {
+    const childrenAsObjects = writeChildrenAsObjects(taggedChildren);
+    const key: string = hashReactChildrenObjects(
+      context ? [childrenAsObjects, context] : childrenAsObjects
+    );
+    return [childrenAsObjects, key];
+  }, [context, taggedChildren]);
+
+  const translation = translations[id];
+
+  if (translation?.promise) {
+    throw new Error(
+      `<T id="${id}">, "${id}" is also used as a key in the dictionary. Don't give <T> components the same ID as dictionary entries.`
+    );
+  }
+  if (!translation?.t || translation.k !== key) {
+    if (
+      process?.env.NODE_ENV === 'development' ||
+      process?.env.NODE_ENV === 'test'
+    ) {
+      throw new Error(
+        `<T id="${id}"> is used in a client component without a valid corresponding translation. This can cause Next.js hydration errors.` +
+          `\n\nYour current environment: "${process?.env.NODE_ENV}". In production, this error will display as a warning only, and content will be rendered in your default language.` +
+          `\n\nTo fix this error, consider using a getGT() dictionary pattern or pushing translations from the command line in advance.`
+      );
+    }
+
+    console.warn(
+      `<T id="${id}"> is used in a client component without a valid corresponding translation.`
+    );
+    const defaultChildren = renderDefaultChildren({
+      children: taggedChildren,
+      variables,
+      variablesOptions,
+      defaultLocale,
+    }) as JSX.Element;
+
+    // The suspense exists here for hydration reasons
+    return <Suspense fallback={defaultChildren}>{defaultChildren}</Suspense>;
+  }
+
+  return renderTranslatedChildren({
+    source: taggedChildren,
+    target: translation.t,
+    variables,
+    variablesOptions,
+    locales: [locale, defaultLocale],
+  }) as JSX.Element;
 }
--- src/internal/addGTIdentifier.tsx
+++ src/internal/addGTIdentifier.tsx
@@ -1,92 +1,115 @@
-import React, { ReactNode, ReactElement, isValidElement } from 'react'
+import React, { ReactNode, ReactElement, isValidElement } from 'react';
 import { isAcceptedPluralForm } from '../primitives/primitives';
 
 type Child = ReactNode;
 type Children = Child[] | Child;
 type GTProp = {
-    id: number,
-    transformation?: string,
-    [key: string]: any;
-}
-
-export default function addGTIdentifier(children: Children, outerID?: string | undefined, startingIndex: number = 0): any {
-
-    // Object to keep track of the current index for GT IDs
-    let index = startingIndex;
-
-    /**
-     * Function to create a GTProp object for a ReactElement
-     * @param child - The ReactElement for which the GTProp is created
-     * @returns - The GTProp object
-     */
-    const createGTProp = (child: ReactElement): GTProp => {
-        const { type, props } = child;
-        index += 1;
-        let result: GTProp = { id: index };
-        let transformation: string;
-        try {
-            transformation = typeof type === 'function' ? ((type as any).gtTransformation || '') : '';
-        } catch (error) {
-            console.error(error)
-            transformation = ''
-        }
-        if (transformation) {
-            const transformationParts = transformation.split('-');
-            if (transformationParts[0] === "variable") {
-                result.variableType = transformationParts?.[1] || "variable";
-            } 
-            if (transformationParts[0] === "plural") {
-                const pluralBranches = Object.entries(props).reduce((acc, [branchName, branch]) => {
-                    if (isAcceptedPluralForm(branchName)) {
-                        (acc as Record<string, any>)[branchName] = addGTIdentifier(branch as any, branchName, index);
-                    }
-                    return acc;
-                }, {});
-                if (Object.keys(pluralBranches).length) result.branches = pluralBranches;
-            }
-            if (transformationParts[0] === "branch") {
-                const { children, branch, ...branches } = props;
-                const resultBranches = Object.entries(branches).reduce((acc, [branchName, branch]) => {
-                    (acc as Record<string, any>)[branchName] = addGTIdentifier(branch as any, branchName, index);
-                    return acc;
-                }, {})
-                if (Object.keys(resultBranches).length) result.branches = resultBranches;
-            }
-            result.transformation = transformationParts[0];
-        }
-        return result;
-    }
-
-    function handleSingleChild(child: any) {
-        if (isValidElement(child)) {
-            const { props } = child as ReactElement;
-            // Create new props for the element, including the GT identifier and a key
-            let generaltranslation = createGTProp(child);  
-            let newProps = {
-                ...props,
-                'data-_gt': generaltranslation
-            };
-            if (outerID) {
-                newProps.key = outerID;
-                outerID = undefined;
-            }
-            // Recursively add IDs to children
-            if (props.children) {
-                newProps.children = handleChildren(props.children);
-            }
-            return React.cloneElement(child, newProps);
-        }
-        return child;
-    }
-    
-    function handleChildren(children: Children) {
-        if (Array.isArray(children)) {
-            outerID = undefined;
-            return React.Children.map(children, handleSingleChild)
-        } else {
-            return handleSingleChild(children);
-        }
-    }
-
-    return handleChildren(children);
+  id: number;
+  transformation?: string;
+  [key: string]: any;
 };
+
+// Recursively adds GT identifiers to the children of a React element
+export default function addGTIdentifier(
+  children: Children,
+  outerID?: string | undefined, // Prevents errors with mapping
+  startingIndex: number = 0
+): any {
+  // Object to keep track of the current index for GT IDs
+  let index = startingIndex;
+
+  /**
+   * Function to create a GTProp object for a ReactElement
+   * Adds all GT info to the object
+   * (This is used to reconstruct the JSX tree post-translation)
+   * @param child - The ReactElement for which the GTProp is created
+   * @returns - The GTProp object
+   */
+  const createGTProp = (child: ReactElement): GTProp => {
+    const { type, props } = child;
+    index += 1;
+    let result: GTProp = { id: index };
+    let transformation: string;
+    try {
+      transformation =
+        typeof type === 'function' ? (type as any).gtTransformation || '' : '';
+    } catch (error) {
+      console.error(error);
+      transformation = '';
+    }
+    if (transformation) {
+      const transformationParts = transformation.split('-');
+      if (transformationParts[0] === 'variable') {
+        result.variableType = transformationParts?.[1] || 'variable';
+      }
+      if (transformationParts[0] === 'plural') {
+        const pluralBranches = Object.entries(props).reduce(
+          (acc, [branchName, branch]) => {
+            if (isAcceptedPluralForm(branchName)) {
+              (acc as Record<string, any>)[branchName] = addGTIdentifier(
+                branch as any,
+                branchName,
+                index
+              );
+            }
+            return acc;
+          },
+          {}
+        );
+        if (Object.keys(pluralBranches).length)
+          result.branches = pluralBranches;
+      }
+      if (transformationParts[0] === 'branch') {
+        const { children, branch, ...branches } = props;
+        const resultBranches = Object.entries(branches).reduce(
+          (acc, [branchName, branch]) => {
+            (acc as Record<string, any>)[branchName] = addGTIdentifier(
+              branch as any,
+              branchName,
+              index
+            );
+            return acc;
+          },
+          {}
+        );
+        if (Object.keys(resultBranches).length)
+          result.branches = resultBranches;
+      }
+      result.transformation = transformationParts[0];
+    }
+    return result;
+  };
+
+  function handleSingleChild(child: any) {
+    if (isValidElement(child)) {
+      const { props } = child as ReactElement;
+      // Create new props for the element, including the GT identifier and a key
+      let generaltranslation = createGTProp(child);
+      let newProps = {
+        ...props,
+        'data-_gt': generaltranslation,
+      };
+      if (outerID) {
+        newProps.key = outerID;
+        outerID = undefined;
+      }
+      // Recursively add IDs to children
+      if (props.children) {
+        newProps.children = handleChildren(props.children);
+      }
+      return React.cloneElement(child, newProps);
+    }
+    return child;
+  }
+
+  function handleChildren(children: Children) {
+    if (Array.isArray(children)) {
+      outerID = undefined;
+      return React.Children.map(children, handleSingleChild);
+    } else {
+      return handleSingleChild(children);
+    }
+  }
+
+  return handleChildren(children);
+}
