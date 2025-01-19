37314a33fd53871131c27a06040be1a2e655ac24
Ernest McCarter <ernest@generaltranslation.com>
2025-01-18 17:06:40 -0800

    useRuntimeTranslation (#46)

--- src/client.ts
+++ src/client.ts
@@ -2,5 +2,5 @@ import { GTContext } from "./provider/GTContext"
 import useBrowserLocale from './hooks/useBrowserLocale'
 import useRuntimeTranslation from "./provider/dynamic/useRuntimeTranslation"
 export {
-    GTContext, useBrowserLocale, useRuntimeTranslation as useDynamicTranslation
+    GTContext, useBrowserLocale, useRuntimeTranslation
 }
